2a2f73afc0b3d5b78fa17191086da96d7435d0f1
Anthony Tilotta <anthonytilotta303@gmail.com>
2021-11-17 15:59:14 -0500

    Update AV-NFT-ROYALTIES.sol

--- AV-NFT-ROYALTIES.sol
+++ AV-NFT-ROYALTIES.sol
@@ -3,48 +3,52 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
 
-//based off of the crypto fax contract
-contract StreamToken is ERC721Full {
-
-    constructor() ERC721Full("StreamToken", "SAV") public { }
+contract ArtRegistry is ERC721Full {
 
     using Counters for Counters.Counter;
     Counters.Counter token_ids;
 
-    struct Art {
-      //Implement artwork struct
-      string hash;
-      uint plays;
-      // ^ represents the number of PLAYS for this ART has based on its HASH
+    struct Artwork {
+        string name;
+        string artist;
+        uint appraisal_value;
     }
+    
+    // ipfs://bafybeify7jbo3zmbyctq33abz5sdxeulfliowkdsxv5ykvaa5ld5vkz3ra
+    // CT ^^^
 
-    // Stores token_id => Art
-    // Only permanent data that you would need to use in a smart contract later should be stored on-chain
-    mapping(uint => Art) public arts;
+    mapping(uint => Artwork) public art_collection;
 
-    // log and report the plays or streams
-    // events allow apps to update and monitor given values on the blockchain
-    event Plays(uint token_id, string report_uri);
+    event Appraisal(uint token_id, uint appraisal_value, string report_uri);
+    
+    constructor() ERC721Full("ExpansiousToken", "EXP") public { }
 
-    function registerArtwork(address owner, string memory hash, string memory token_uri) public returns(uint) {
+    function registerArtwork(
+        address owner, 
+        string memory name, 
+        string memory artist, 
+        uint initial_value, 
+        string memory token_uri
+    )   
+        public 
+        returns(uint) 
+    {
         token_ids.increment();
         uint token_id = token_ids.current();
 
         _mint(owner, token_id);
         _setTokenURI(token_id, token_uri);
 
-        arts[token_id] = Art(hash, 0);
+        art_collection[token_id] = Artwork(name, artist, initial_value);
 
         return token_id;
     }
 
-    function recordStream(uint token_id, string memory report_uri) public returns(uint) {
-       //Implement recordStream
-       arts[token_id].plays += 1;
-       
-       // permanently associates the report_uri with the token_id on-chain via Events for a lower gas-cost than storing directly within this contract
-       emit Plays(token_id, report_uri);
-       
-       return arts[token_id].plays;
+    function newAppraisal(uint token_id, uint new_value, string memory report_uri) public returns(uint) {
+        art_collection[token_id].appraisal_value = new_value;
+
+        emit Appraisal(token_id, new_value, report_uri);
+
+        return art_collection[token_id].appraisal_value;
     }
 }
